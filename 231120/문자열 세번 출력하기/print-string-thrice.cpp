60826507cb757cf7fd81058913a6280cf4c44e9b
DAZB <123790245+D-A-Z-B@users.noreply.github.com>
2023-11-20 18:31:03 +0900

    Title: 문자열 세번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/문자열 세번 출력하기/print-string-thrice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    for (int i =0; i < 3; ++i) {
+        cout << str << endl;
+    }
+    return 0;
+}
