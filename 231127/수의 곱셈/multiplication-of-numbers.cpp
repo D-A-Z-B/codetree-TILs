a1a00044a6c681bf4d85e70f03c9b54d27ea05fd
DAZB <123790245+D-A-Z-B@users.noreply.github.com>
2023-11-27 20:02:45 +0900

    Title: 수의 곱셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231127/수의 곱셈/multiplication-of-numbers.cpp
+++ 231127/수의 곱셈/multiplication-of-numbers.cpp
@@ -6,7 +6,25 @@ int main()
 {
     int a[3];
     cin >> a[0] >> a[1] >> a[2];
-    sort(a, a + 3);
-    int answer = max({ a[0] * a[1], a[0] * a[2], a[1] * a[2], a[0] * a[1] * a[2] });
+    bool isAllEven = true;
+    int answer = 1;
+    for (int i = 0; i < 3; ++i) {
+        if (a[i] % 2 != 0) {
+            isAllEven = false;
+            break;
+        }
+    }
+
+    if (isAllEven) {
+        for (int i = 0; i < 3; ++i) {
+            answer *= a[i];
+        }
+    } else {
+        for (int i = 0; i < 3; ++i) {
+            if (a[i] % 2 != 0) {
+                answer *= a[i];
+            }
+        }
+    }
     cout << answer;
 }
