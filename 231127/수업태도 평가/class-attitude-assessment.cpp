eb68912e394ac770028847af12d862b5d1cb2402
DAZB <123790245+D-A-Z-B@users.noreply.github.com>
2023-11-27 19:19:31 +0900

    Title: 수업태도 평가, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231127/수업태도 평가/class-attitude-assessment.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    map<string, int> m;
+    m.insert({ "Bessie", 0 });
+    m.insert({ "Elsie", 0 });
+    m.insert({ "Daisy", 0 });
+    m.insert({ "Gertie", 0 });
+    m.insert({ "Annabelle", 0 });
+    m.insert({ "Maggie", 0 });
+    m.insert({ "Henrietta", 0 });
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        string name;
+        int score;
+        cin >> name >> score;
+        auto student = m.find(name);
+        if (student != m.end()) {
+            student->second += score;
+        }
+    }
+
+    vector<pair<string, int>> sortedMap(m.begin(), m.end());
+    sort(sortedMap.begin(), sortedMap.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second;
+        });
+
+    int smallestValue = sortedMap[0].second;
+    int secondSmallestValue = -1;
+
+    for (const auto& entry : sortedMap) {
+        if (entry.second > smallestValue) {
+            secondSmallestValue = entry.second;
+            break;
+        }
+    }
+
+    if (secondSmallestValue != -1) {
+        for (const auto& entry : sortedMap) {
+            if (entry.second == secondSmallestValue) {
+                cout << entry.first << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
